369ef2672a2bbc737659b1ccff4ba552cccceae3
Nicola Mocci <nicolamocci.nm@gmail.com>
2025-10-01 16:18:58 +0200

    exercise best_offer

---
+++ 1/best-offer/solution/_build/default/bin/main.ml
@@ -0,0 +1,10 @@
+
+
+let best_offer (o1:int option) (o2:int option) (o3:int option) = 
+  let lst = List.filter_map (fun x -> x) [o1;o2;o3] in
+  match lst with
+  | [] -> None
+  | x::xs -> Some(List.fold_left max x xs);;
+
+
+let () = print_endline "Hello, World!"
